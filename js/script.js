f0c22704c44e2fd86b7e125e9423bf5643829d62
Dariusz Franczak <darius32@op.pl>
2017-05-10 15:05:39 +0100

    main controller and mn-model set up

--- js/script.js
+++ js/script.js
@@ -2,9 +2,21 @@
 
     var app = angular.module('githubViewer', []);
 
-    var MainController = function($scope) {
+    var MainController = function($scope, $http) {
 
-        $scope.message = 'This message comes from Main Controller';
+        var onUserComplete = function(responce) {
+            $scope.user = responce.data;
+        };
+
+        var onError = function(reason) {
+            $scope.error = "Could not fetch the user. Please check if you've spelled Username correctly.";
+        };
+
+        $http.get('https://api.github.com/users/dariusz75')
+        .then(onUserComplete, onError);
+
+        $scope.username = 'Angular';
+        $scope.message = 'GitHub Viewer';
 
     };
 
